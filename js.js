2e08df1953687f131b80ed4e1658dfc5556586c7
Thais Franca <tfrancag@gmail.com>
2021-11-12 16:49:10 -0300

    Agora pelo menos as mensagens estão carregando - uma vez só - de forma correta. O layout ainda não está correto, mas as mensagens estão sendo recolhidas do servidor

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 nomeUsuario();
 function nomeUsuario(){
 // let nomeUsuarioFornecido = prompt('Selecione um nome de usuário!');
-let nomeUsuarioFornecido = 'Julio';
+let nomeUsuarioFornecido = 'Jfdsfs';
 // pedir o nome de usuário para o cliente
 // transformar o nome de usuário em objeto, para poder ser comparado no servidor
 
@@ -27,12 +27,40 @@ function logMensagens(){
 }
 function gerarMensagens(resposta){
 console.log(resposta.data);
-console.log(resposta.data[1]);
+console.log(resposta.data[1].from);
+console.log(resposta.data[1].to);
+console.log(resposta.data[1].type);
+console.log(resposta.data[1].text);
+console.log(resposta.data[1].time);
+//esse é o formato para que eu consiga acessar as informaçòes dentro dos objetos
 
 for(let ii=0; ii<resposta.data.length;ii++){
+    //com isso, ele vai passar por todos os objetos contidos
     let conteudo = document.querySelector(".conteudo");
-    conteudo.innerHTML +=` <span>${resposta.data[ii].text}<span>`
-    console.log(conteudo);
+    //porque é dentro do espaço de conteúdo que serão escritas as mensagens
+    //as agora eu tenho 4 itens para pegar, sendo que um deles, o status, eu tenho que conferir dentro de um if para ter certeza de qual a classe correta
+    let tipoMensagem = resposta.data[ii].type;
+    let tipoDeMensagem = "";
+    if (tipoMensagem == 'status'){
+        tipoDeMensagem = "status";
+        conteudo.innerHTML +=` <div class = "mensagem-geral ${tipoDeMensagem}"> <span class = "tempo">${resposta.data[ii].time}</span>
+        <span class = "from">${resposta.data[ii].from}</span>
+        <span class = "mensagem">${resposta.data[ii].text}</span></div>`;
+    }
+
+    else{
+    if (tipoMensagem == "message"){
+        tipoDeMensagem = "mensagem-geral";  
+    }
+    else if (tipoMensagem == "private_message"){
+        tipoDeMensagem = "mensagem-geral privada";
+    }
+    conteudo.innerHTML +=` <div class = ${tipoDeMensagem}> <span class = "tempo">${resposta.data[ii].time}</span>
+    <span class = "from">${resposta.data[ii].from}</span>
+    <span>para</span>
+    <span class = "to">${resposta.data[ii].to}</span>
+    <span class = "mensagem">${resposta.data[ii].text}</span></div>`;
+}
 }
 }
 
