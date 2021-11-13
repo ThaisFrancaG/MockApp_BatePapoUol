8266e3c6b218f276dd02fb5949bb8244945d999d
Thais Franca <tfrancag@gmail.com>
2021-11-13 16:41:48 -0300

    finalmente as mensagens estão carregando só a partir de certo ponto, sem repetir todas as mensagens todas as vezes. Provavelmente não foi feito da melhor forma, mas foi feito de uma forma que funciona sem armazenar informação excessiva

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 nomeUsuario();
 function nomeUsuario(){
 // let nomeUsuarioFornecido = prompt('Selecione um nome de usuário!');
-let nomeUsuarioFornecido = 'Jfdsfs';
+let nomeUsuarioFornecido = 'Jfdsfsffds';
 // pedir o nome de usuário para o cliente
 // transformar o nome de usuário em objeto, para poder ser comparado no servidor
 
@@ -20,19 +20,44 @@ function nomeValido(){
     alert('bem-vindo ao chat');
     logMensagens();
 }
+// -----------------------------------------------------------------------------------------------PEGAR MENSAGENS----------------------------//
+
+//para testest
+let logMensagensTeste;
+setInterval(logMensagens,10000);
+//A CADA 10 SEGUNDOS, ESSA FUNÇAO VAI CHAMAR O LOG MENSAGENS, O QUAL ACESSA O SERVIDORE E VÊ TODAS AS MENSAGENS QUE ESTÀO LÁ DENTRO
+let ultimaMensagem  = {};
+//LITERALMENTE SÓ VAI SER VAZIA NO MOMENTO EM QUE A PÁGINA CARREGA. EU A ESTOU COLOCANDO AQUI FORA PORQUE QUERO QUE ELA SEJA GLOBAL E POSSA ANDAR ENTRE AS FUNÇÕES
+logMensagens();
 
 function logMensagens(){
     let mensagensAnterior = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
+
     mensagensAnterior.then(gerarMensagens);
+//A FUNÇÀO PEGA TODAS AS MENSAGENS QUE ESTÀO DENTRO DO SERVIDOR, E QUANDO ESSAS MENSAGENS CHEGAM, ELE CHAMA OUTRA FUNÇAO, QUE LÊ OS DADOS E OS ESCREVE
+
+//IDEIA: EU POSSO CRIAR UMA FUNÇÃO QUE OPERA ANTES DA GERAÇÀO DE MENSAGENS, PARA QUE ESTAS SEJAM TRATADAS?
 }
+
 function gerarMensagens(resposta){
-console.log(resposta.data);
-console.log(resposta.data[1].from);
-console.log(resposta.data[1].to);
-console.log(resposta.data[1].type);
-console.log(resposta.data[1].text);
-console.log(resposta.data[1].time);
-//esse é o formato para que eu consiga acessar as informaçòes dentro dos objetos
+
+console.log(resposta.data.length);
+
+if(Object.keys(ultimaMensagem).length !== 0){
+    console.log("ok");
+    //isso me diz se a última mensagem está vazia ou não
+    let indexSlice = resposta.data.indexOf(ultimaMensagem);
+    if(resposta.data[indexSlice]===ultimaMensagem){
+        console.log("não devia imprimir nada");
+        resposta.data ={};
+        //esvazaio o objeto pra não rolar nada
+    }
+    else{
+    resposta.data = resposta.data.slice(indexSlice);
+    console.log(resposta.data.length);}
+}
+else{console.log("errou");}
+
 
 for(let ii=0; ii<resposta.data.length;ii++){
     //com isso, ele vai passar por todos os objetos contidos
@@ -52,9 +77,12 @@ for(let ii=0; ii<resposta.data.length;ii++){
     if (tipoMensagem == "message"){
         tipoDeMensagem = "mensagem-geral";  
     }
-    else if (tipoMensagem == "private_message"){
+    else if (tipoMensagem == "private_message" && resposta.data[ii].to== nomeUsuarioFornecido)
+    //tem que corrigir esse condicional depois
+    {
         tipoDeMensagem = "mensagem-geral privada";
     }
+    else{tipoMensagem == "hidden"}
     conteudo.innerHTML +=` <div class = ${tipoDeMensagem}> <span class = "tempo">${resposta.data[ii].time}</span>
     <span class = "from">${resposta.data[ii].from}</span>
     <span>para</span>
@@ -62,6 +90,11 @@ for(let ii=0; ii<resposta.data.length;ii++){
     <span class = "mensagem">${resposta.data[ii].text}</span></div>`;
 }
 }
+// fecha o for
+console.log(resposta.data.length-1);
+ultimaMensagem = resposta.data[resposta.data.length-1];
+console.log(ultimaMensagem);
+
 }
 
 function enviarMensagem(){
